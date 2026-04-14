1a3440e4aa177b8ab2638ce162f3a869e0645622
Meet-Rathod-3898 <meetrathod474@gmail.com>
2026-04-14 20:02:59 +0530

    Add files via upload

---
+++ PHP/02_store.php
@@ -0,0 +1,7 @@
+<?php
+$name="Meet";
+$age=18;
+echo"user details<br>";
+echo"name:$name<br>";
+echo"age:$age<br>";
+?>
---
+++ PHP/03_swap.php
@@ -0,0 +1,8 @@
+<?php
+$a=30;
+$b=50;
+echo"<br>before swapping <br>";
+echo"<br>$a=30<br>";
+echo"<br>$b=50<br>";
+
+// logic of swap
